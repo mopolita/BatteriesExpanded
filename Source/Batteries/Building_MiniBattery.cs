fd1df2e5fa3cf7380fec20d8586998ee36ec05ec
mopolita <albanrichard48@gmail.com>
2023-10-09 21:18:27 +0200

    finally, mini batteries work !

--- Source/Batteries/Building_MiniBattery.cs
+++ Source/Batteries/Building_MiniBattery.cs
@@ -1,8 +1,47 @@
 ﻿using System;
+using System.Reflection;
 using RimWorld;
 using UnityEngine;
 using Verse;
 
 namespace Batteries
 {
+    public class Building_Minibattery : Building_Battery
+    {
+        private static readonly Vector2 BarSize = new Vector2(0.4f, 0.4f);
+
+        private Vector2 GetBarSize()
+        {
+            FieldInfo field = typeof(Building_Battery).GetField("BarSize", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.GetField);
+            if (field == null)
+            {
+                return Vector2.zero;
+            }
+            object value = field.GetValue(this);
+            return (Vector2)value;
+        }
+
+        private void SetBarSize(Vector2 barSize)
+        {
+            FieldInfo field = typeof(Building_Battery).GetField("BarSize", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.SetField);
+            if (!(field == null))
+            {
+                field.SetValue(this, barSize);
+            }
+        }
+
+        public override void Draw()
+        {
+            Vector2 barSize = GetBarSize();
+            SetBarSize(BarSize);
+            try
+            {
+                base.Draw();
+            }
+            finally
+            {
+                SetBarSize(barSize);
+            }
+        }
+    }
 }

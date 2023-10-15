607fbd8102fffdb8290149dd9c7b80ba134f522c
mopolita <albanrichard48@gmail.com>
2023-10-15 19:20:58 +0200

    Revert work in source

--- Source/Batteries/Building_MiniBattery.cs
+++ Source/Batteries/Building_MiniBattery.cs
@@ -6,27 +6,10 @@ using Verse;
 
 namespace Batteries
 {
-    public class BarSize : DefModExtension
-    {
-        public float sizeX;
-        public float sizeY;
-    }
-
     public class Building_Minibattery : Building_Battery
     {
-        private static readonly Vector2 BarSize = new Vector2(0.4f,0.4f);
+        private static readonly Vector2 BarSize = new Vector2(0.4f, 0.4f);
 
-        private static Vector2 ChangeBarSize(ThingDef thingDef) 
-        {
-            if (thingDef.HasModExtension<BarSize>())
-            {
-                float sizeX = thingDef.GetModExtension<BarSize>().sizeX;
-                float sizeY = thingDef.GetModExtension<BarSize>().sizeY;
-                return new Vector2(sizeX, sizeY);
-            }
-            return Vector2.zero;
-        }
-      
         private Vector2 GetBarSize()
         {
             FieldInfo field = typeof(Building_Battery).GetField("BarSize", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.GetField);
@@ -41,7 +24,7 @@ namespace Batteries
         private void SetBarSize(Vector2 barSize)
         {
             FieldInfo field = typeof(Building_Battery).GetField("BarSize", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.SetField);
-            if (field != null)
+            if (!(field == null))
             {
                 field.SetValue(this, barSize);
             }

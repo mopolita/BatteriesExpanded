c74109480bcbae40f2bb74f8c8baf93edb07aecc
mopolita <albanrichard48@gmail.com>
2023-11-04 00:02:10 +0100

    code update finished

--- Source/Batteries/Building_CustomBattery.cs
+++ Source/Batteries/Building_CustomBattery.cs
@@ -10,32 +10,38 @@ namespace Batteries
     {
         public ModExtension_BatterySize Props => def.GetModExtension<ModExtension_BatterySize>();
 
-        private void SetBarSize()
+        private static Vector2 BarSize = new Vector2(0.4f, 0.4f);
+
+        private Vector2 GetBarSize()
+        {
+            FieldInfo field = typeof(Building_Battery).GetField("BarSize", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.GetField);
+            if (field == null)
+            {
+                return Vector2.zero;
+            }
+            object value = field.GetValue(this);
+            return (Vector2)value;
+        }
+        private void SetBarSize(Vector2 barSize)
         {
             FieldInfo field = typeof(Building_Battery).GetField("BarSize", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.SetField);
-            if (field != null)
-            {
-                if (Props != null)
-                {
-                    field.SetValue(this, new Vector2(Props.sizeX, Props.sizeY));
-                }
-                else
-                {
-                    field.SetValue(this, new Vector2(1.3f, 0.4f));
-                }
-            }
+            field?.SetValue(this, barSize);
         }
-
         public override void Draw()
         {
-            SetBarSize();
+            if (Props != null)
+            {
+                BarSize = new Vector2(Props.sizeX, Props.sizeY);
+            }
+            Vector2 barSize = GetBarSize();
+            SetBarSize(BarSize);
             try
             {
                 base.Draw();
             }
             finally
             {
-                SetBarSize();
+                SetBarSize(barSize);
             }
         }
     }

840d458fd949b3f2f0d9b44208b9d6fc734a6826
mopolita <albanrichard48@gmail.com>
2024-08-07 00:35:35 +0200

    fixed battery bar sizing error

--- Source/Batteries/BatterySize.cs
+++ Source/Batteries/BatterySize.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Verse;
+﻿using Verse;
 
 namespace Batteries
 {
-    public class ModExtension_BatterySize : DefModExtension
+    public class ModExtension_BarSize : DefModExtension
     {
         public float sizeX;
         public float sizeY;
--- Source/Batteries/Building_CustomBattery.cs
+++ Source/Batteries/Building_CustomBattery.cs
@@ -1,48 +1,109 @@
-﻿using System;
-using System.Reflection;
-using RimWorld;
+﻿using RimWorld;
 using UnityEngine;
 using Verse;
+using Verse.Sound;
 
 namespace Batteries
 {
-    public class Building_CustomBattery : Building_Battery
+    public class Building_CustomBattery : Building
     {
-        public ModExtension_BatterySize Props => def.GetModExtension<ModExtension_BatterySize>();
+        private const float MinEnergyToExplode = 250f;
+        private const float EnergyToLoseWhenExplode = 200f;
+        private const float ExplodeChancePerDamage = 0.05f;
+        
+        public ModExtension_BarSize Props => def.GetModExtension<ModExtension_BarSize>();
 
-        private static Vector2 BarSize = new Vector2(0.4f, 0.4f);
+        private static readonly Vector2 BarSize = new Vector2(1.3f, 0.4f);
 
-        private Vector2 GetBarSize()
+        private static readonly Material BatteryBarFilledMat =
+            SolidColorMaterials.SimpleSolidColorMaterial(new Color(0.9f, 0.85f, 0.2f));
+
+        private static readonly Material BatteryBarUnfilledMat =
+            SolidColorMaterials.SimpleSolidColorMaterial(new Color(0.3f, 0.3f, 0.3f));
+
+        private int ticksToExplode;
+        private Sustainer wickSustainer;
+
+        public override void ExposeData()
         {
-            FieldInfo field = typeof(Building_Battery).GetField("BarSize", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.GetField);
-            if (field == null)
+            base.ExposeData();
+            Scribe_Values.Look(ref ticksToExplode, "ticksToExplode");
+        }
+
+        public override void Tick()
+        {
+            base.Tick();
+            if (ticksToExplode <= 0)
             {
-                return Vector2.zero;
+                return;
             }
-            object value = field.GetValue(this);
-            return (Vector2)value;
+
+            if (wickSustainer == null)
+            {
+                StartWickSustainer();
+            }
+            else
+            {
+                wickSustainer.Maintain();
+            }
+
+            ticksToExplode--;
+            if (ticksToExplode != 0)
+            {
+                return;
+            }
+
+            var radius = Rand.Range(0.5f, 1f) * 1.5f;
+            GenExplosion.DoExplosion(Rotation.FacingCell, Map, radius, DamageDefOf.Flame, null);
+            GetComp<CompPowerBattery>().DrawPower(EnergyToLoseWhenExplode);
         }
-        private void SetBarSize(Vector2 barSize)
+
+        public override void PostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
         {
-            FieldInfo field = typeof(Building_Battery).GetField("BarSize", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.SetField);
-            field?.SetValue(this, barSize);
+            base.PostApplyDamage(dinfo, totalDamageDealt);
+            if (Destroyed || ticksToExplode != 0 || dinfo.Def != DamageDefOf.Flame ||
+                !(Rand.Value < ExplodeChancePerDamage) ||
+                !(GetComp<CompPowerBattery>().StoredEnergy > MinEnergyToExplode))
+            {
+                return;
+            }
+
+            ticksToExplode = Rand.Range(70, 150);
+            StartWickSustainer();
         }
-        public override void Draw()
+
+        private void StartWickSustainer()
         {
+            SoundInfo info = SoundInfo.InMap(this, MaintenanceType.PerTick);
+            wickSustainer = SoundDefOf.HissSmall.TrySpawnSustainer(info);
+        }
+       
+        public override void DrawAt(Vector3 drawLoc, bool flip = false)
+        {
+            base.DrawAt(drawLoc, flip);
+            CompPowerBattery comp = GetComp<CompPowerBattery>();
+            GenDraw.FillableBarRequest fillableBarRequest = default;
+            
             if (Props != null)
+                fillableBarRequest.size = new Vector2(Props.sizeX, Props.sizeY);
+            else
+                fillableBarRequest.size = BarSize;
+            
+            fillableBarRequest.center = drawLoc + Vector3.up * 0.1f;
+            fillableBarRequest.fillPercent = comp.StoredEnergy / comp.Props.storedEnergyMax;
+            fillableBarRequest.filledMat = BatteryBarFilledMat;
+            fillableBarRequest.unfilledMat = BatteryBarUnfilledMat;
+            fillableBarRequest.margin = 0.15f;
+            GenDraw.FillableBarRequest r = fillableBarRequest;
+            Rot4 rotation = base.Rotation;
+            rotation.Rotate(RotationDirection.Clockwise);
+            r.rotation = rotation;
+            GenDraw.DrawFillableBar(r);
+            if (ticksToExplode > 0 && base.Spawned)
             {
-                BarSize = new Vector2(Props.sizeX, Props.sizeY);
-            }
-            Vector2 barSize = GetBarSize();
-            SetBarSize(BarSize);
-            try
-            {
-                base.Draw();
-            }
-            finally
-            {
-                SetBarSize(barSize);
+                base.Map.overlayDrawer.DrawOverlay(this, OverlayTypes.BurningWick);
             }
         }
+
     }
 }
